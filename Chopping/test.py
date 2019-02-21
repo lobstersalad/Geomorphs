d6d1964ec4e9bdd1d34bc5d61f6de5c5ae5d4f9c
Myron Kant <mkant@mail.csuchico.edu>
2019-02-21 11:09:57 -0800

    Added command to recombine chopped image

--- Chopping/test.py
+++ Chopping/test.py
@@ -26,8 +26,16 @@ def main(argv):
     print ('Input file is', input)
     print ('Output file is', output)
     print ('Dimensions are', dimensions[:-1])
-    # convert test_leaf.png: -crop 3x3@ +repage +adjoin test_leaf_3x3@_%d.png
+
+    # the following ImageMagick command will chop up input image with provided specifications
+    # convert test_leaf.png: -crop 3x3@ +repage +adjoin test_leaf_tile_3x3@_%d.png
     command = ['convert', input + '.png', '-crop', dimensions, '+repage', '+adjoin', output + '_' + dimensions + '_%d.png']
+    subprocess.call(command, shell = False)
+
+
+    # the following ImageMagick command will recombine image tiles to form original image, including any modifications
+    # montage test_leaf_tile*.png -mode Concatenate -tile 3x3 recombined.png
+    command = ['montage', output + '*.png', '-mode', 'Concatenate', '-tile', dimensions[:-1], 'recombined.png']
     subprocess.call(command, shell = False)
 
 if __name__ == "__main__":
