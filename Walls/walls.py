0d569f00c0474406162032810dbc6ac8efc5ca35
Myron Kant <mkant@mail.csuchico.edu>
2019-03-14 16:20:35 -0700

    Changed canny edge detection for walls

--- Walls/walls.py
+++ Walls/walls.py
@@ -16,21 +16,17 @@ cv2.imwrite('dilated_test_dungeon.jpg', dilated)
 # Convert image to gray colourspace
 img_bw = cv2.cvtColor(dilated, cv2.COLOR_BGR2GRAY)
 
-# Canny edge detection to find walls of dungeon doesn't work very well
-'''
+# Canny edge detection works very well
 tight = cv2.Canny(dilated, 225, 250)
+cv2.imwrite('cannytight_test_dungeon.jpg', tight)
 wide = cv2.Canny(dilated, 10, 200)
+cv2.imwrite('cannywide_test_dungeon.jpg', wide)
 
-# Display canny edge detection results
-plt.subplot(121), plt.imshow(tight, cmap = 'gray')
-plt.title('Tight'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(wide, cmap = 'gray')
-plt.title('Wide'), plt.xticks([]), plt.yticks([])
-'''
-
-# Using contours to find walls instead
+# Contours also work well to find edges
 (thresh, img_bw) = cv2.threshold(img_bw, 128, 255, 0)
 cv2.imwrite('bw_test_dungeon.jpg', img_bw)
 contours, hierarchy = cv2.findContours(img_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(dilated, contours, -1, (0, 255, 0), 2)
 cv2.imwrite('cnt_test_dungeon.jpg', dilated)
+
+plt.show()
