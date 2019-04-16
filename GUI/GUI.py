b8e3f2801943171aad8c28008e19f46acafac43b
Myron Kant <mkant@mail.csuchico.edu>
2019-04-15 18:27:09 -0700

    Interfacing with website via GUI dropdowns working

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,81 +1,108 @@
 # Sources: http://zetcode.com/gui/pyqt5
 #          https://riverbankcomputing.com/static/Docs/PyQt5
+#          https://pythonprogramminglanguage.com/pyqt5-button/
 
+import sys
+sys.path.append('../Texturing/')
+
+from downloader import download
+from texturer import texture
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QGridLayout, QComboBox, QPushButton
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QLabel, QGridLayout, QComboBox, QPushButton
 
-app = QApplication([])
-window = QWidget()
-grid = QGridLayout()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
 
-dungeon_layout_label = QLabel("Dungeon Layout")
-dungeon_layout_dd = QtWidgets.QComboBox()
-options = ["Layout", "Square", "Rectangle", "Box", "Cross", "Dagger", "Saltire", "Keep", "Hexagon", "Round", "Cavernous"]
-dungeon_layout_dd.addItems(options)
-grid.addWidget(dungeon_layout_label, 1, 0)
-grid.addWidget(dungeon_layout_dd, 1, 1)
+        window = QWidget(self)
+        self.setCentralWidget(window)
+        grid = QGridLayout(window)
 
-dungeon_size_label = QLabel("Dungeon Size")
-dungeon_size_dd = QtWidgets.QComboBox()
-options = ["Fine", "Diminiutive", "Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan", "Colossal"]
-# dungeon_size_dd.addItem("Custom") needs to open new dimension text boxes
-dungeon_size_dd.addItems(options)
-grid.addWidget(dungeon_size_label, 2, 0)
-grid.addWidget(dungeon_size_dd, 2, 1)
+        dungeon_layout_label = QLabel("Dungeon Layout")
+        self.dungeon_layout_dd = QtWidgets.QComboBox()
+        options = ["Layout", "Square", "Rectangle", "Box", "Cross", "Dagger", "Saltire", "Keep", "Hexagon", "Round", "Cavernous"]
+        self.dungeon_layout_dd.addItems(options)
+        grid.addWidget(dungeon_layout_label, 1, 0)
+        grid.addWidget(self.dungeon_layout_dd, 1, 1)
 
-peripheral_egress_label = QLabel("Peripheral Egress")
-peripheral_egress_dd = QtWidgets.QComboBox()
-options = ["No", "Yes", "Tiling"]
-peripheral_egress_dd.addItems(options)
-grid.addWidget(peripheral_egress_label, 3, 0)
-grid.addWidget(peripheral_egress_dd, 3, 1)
+        dungeon_size_label = QLabel("Dungeon Size")
+        self.dungeon_size_dd = QtWidgets.QComboBox()
+        options = ["Fine", "Diminiutive", "Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan", "Colossal"]
+        # dungeon_size_dd.addItem("Custom") would need to open new dimension text boxes
+        self.dungeon_size_dd.addItems(options)
+        grid.addWidget(dungeon_size_label, 2, 0)
+        grid.addWidget(self.dungeon_size_dd, 2, 1)
 
-add_stairs_label = QLabel("Stairs")
-add_stairs_dd = QtWidgets.QComboBox()
-options = ["No", "Yes", "Many"]
-add_stairs_dd.addItems(options)
-grid.addWidget(add_stairs_label, 4, 0)
-grid.addWidget(add_stairs_dd, 4, 1)
+        peripheral_egress_label = QLabel("Peripheral Egress")
+        self.peripheral_egress_dd = QtWidgets.QComboBox()
+        options = ["No", "Yes", "Tiling"]
+        self.peripheral_egress_dd.addItems(options)
+        grid.addWidget(peripheral_egress_label, 3, 0)
+        grid.addWidget(self.peripheral_egress_dd, 3, 1)
 
-room_layout_label = QLabel("Room Layout")
-room_layout_dd = QtWidgets.QComboBox()
-options = ["Sparse", "Scattered", "Dense", "Symmetric", "Complex"]
-room_layout_dd.addItems(options)
-grid.addWidget(room_layout_label, 5, 0)
-grid.addWidget(room_layout_dd, 5, 1)
+        add_stairs_label = QLabel("Stairs")
+        self.add_stairs_dd = QtWidgets.QComboBox()
+        options = ["No", "Yes", "Many"]
+        self.add_stairs_dd.addItems(options)
+        grid.addWidget(add_stairs_label, 4, 0)
+        grid.addWidget(self.add_stairs_dd, 4, 1)
 
-room_size_label = QLabel("Room Size")
-room_size_dd = QtWidgets.QComboBox()
-options = ["Small", "Medium", "Large", "Huge", "Gargantuan", "Colossal"]
-room_size_dd.addItems(options)
-grid.addWidget(room_size_label, 6, 0)
-grid.addWidget(room_size_dd, 6, 1)
+        room_layout_label = QLabel("Room Layout")
+        self.room_layout_dd = QtWidgets.QComboBox()
+        options = ["Sparse", "Scattered", "Dense", "Symmetric", "Complex"]
+        self.room_layout_dd.addItems(options)
+        grid.addWidget(room_layout_label, 5, 0)
+        grid.addWidget(self.room_layout_dd, 5, 1)
 
-door_set_label = QLabel("Doors")
-door_set_dd = QtWidgets.QComboBox()
-options = ["None", "Basic", "Secure", "Standard", "Deathtrap"]
-door_set_dd.addItems(options)
-grid.addWidget(door_set_label, 7, 0)
-grid.addWidget(door_set_dd, 7, 1)
+        room_size_label = QLabel("Room Size")
+        self.room_size_dd = QtWidgets.QComboBox()
+        options = ["Small", "Medium", "Large", "Huge", "Gargantuan", "Colossal"]
+        self.room_size_dd.addItems(options)
+        grid.addWidget(room_size_label, 6, 0)
+        grid.addWidget(self.room_size_dd, 6, 1)
 
-corridor_layout_label = QLabel("Corridors")
-corridor_layout_dd = QtWidgets.QComboBox()
-options = ["Labyrinth", "Errant", "Straight"]
-corridor_layout_dd.addItems(options)
-grid.addWidget(corridor_layout_label, 8, 0)
-grid.addWidget(corridor_layout_dd, 8, 1)
+        door_set_label = QLabel("Doors")
+        self.door_set_dd = QtWidgets.QComboBox()
+        options = ["None", "Basic", "Secure", "Standard", "Deathtrap"]
+        self.door_set_dd.addItems(options)
+        grid.addWidget(door_set_label, 7, 0)
+        grid.addWidget(self.door_set_dd, 7, 1)
 
-remove_deadends_label = QLabel("Remove Deadends")
-remove_deadends_dd = QtWidgets.QComboBox()
-options = ["None", "Some", "All"]
-remove_deadends_dd.addItems(options)
-grid.addWidget(remove_deadends_label, 9, 0)
-grid.addWidget(remove_deadends_dd, 9, 1)
+        corridor_layout_label = QLabel("Corridors")
+        self.corridor_layout_dd = QtWidgets.QComboBox()
+        options = ["Labyrinth", "Errant", "Straight"]
+        self.corridor_layout_dd.addItems(options)
+        grid.addWidget(corridor_layout_label, 8, 0)
+        grid.addWidget(self.corridor_layout_dd, 8, 1)
 
-construct = QPushButton("Construct")
-construct.clicked.connect()
+        remove_deadends_label = QLabel("Remove Deadends")
+        self.remove_deadends_dd = QtWidgets.QComboBox()
+        options = ["None", "Some", "All"]
+        self.remove_deadends_dd.addItems(options)
+        grid.addWidget(remove_deadends_label, 9, 0)
+        grid.addWidget(self.remove_deadends_dd, 9, 1)
 
-window.setLayout(grid)
-window.show()
+        construct = QPushButton("Download")
+        construct.clicked.connect(self.download_button)
+        grid.addWidget(construct, 10, 0)
 
-app.exec_()
+        construct = QPushButton("Texture")
+        construct.clicked.connect(texture)
+        grid.addWidget(construct, 10, 1)
+
+    def download_button(self):
+        download(self.dungeon_layout_dd.currentText(),
+                 self.dungeon_size_dd.currentText(),
+                 self.peripheral_egress_dd.currentText(),
+                 self.add_stairs_dd.currentText(),
+                 self.room_layout_dd.currentText(),
+                 self.room_size_dd.currentText(),
+                 self.door_set_dd.currentText(),
+                 self.corridor_layout_dd.currentText(),
+                 self.remove_deadends_dd.currentText())
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
---
+++ GUI/downloader.py
@@ -0,0 +1,68 @@
+# Sources: https://selenium-python.readthedocs.io/
+#          https://stackoverflow.com/questions/45631715/downloading-with-chrome-headless-and-selenium
+
+'''
+ToDo
+- Rename downloaded file
+'''
+
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def download(dungeon_layout, dungeon_size, peripheral_egress, add_stairs,
+             room_layout, room_size, door_set, corridor_layout, remove_deadends):
+    def text_select(option, value):
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, option)), message = "Unable to Locate Text Box")
+        element.clear()
+        element.send_keys(value)
+
+    def dd_select(option, value):
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, option)), message = "Unable to Locate Dropdown")
+        select = Select(element)
+        select.select_by_visible_text(value)
+
+    def button_press(option, wait):
+        element = WebDriverWait(driver, wait).until(EC.presence_of_element_located((By.XPATH, option)), message = "Unable to Locate Button")
+        element.click()
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome(chrome_options = options)
+    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': '/home/lobstersalad/Documents/490/Geomorphs/Texturing'}}
+    driver.execute("send_command", params)
+    print ("Connecting to donjon...")
+    driver.get("https://donjon.bin.sh/d20/dungeon/")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "dungeon_form")), message = "Connection Failed")
+
+    # Don't really care about level, motif
+    # Need defaults for style, grid
+    print ("Selecting Map Options...")
+    text_select("name", "Testing")
+    dd_select("level", "1")
+    dd_select("motif", "None")
+    text_select("seed", "34034853")
+    dd_select("map_style", "Standard")
+    dd_select("grid", "Square")
+    dd_select("dungeon_layout", dungeon_layout)
+    dd_select("dungeon_size", dungeon_size)
+    dd_select("peripheral_egress", peripheral_egress)
+    dd_select("add_stairs", add_stairs)
+    dd_select("room_layout", room_layout)
+    dd_select("room_size", room_size)
+    dd_select("door_set", door_set)
+    dd_select("corridor_layout", corridor_layout)
+    dd_select("remove_deadends", remove_deadends)
+
+    button_press("//input[@value = 'Construct']", 10)
+    print ("Constructing Map...")
+
+    button_press("//input[@value = 'Print Map']", 600)
+    print ("Downloading Map!")
+
+if __name__ == "__download__":
+    download()
